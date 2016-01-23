06ba3d0476fd7ba2580c6373e82a2e774b37b557
Jim Allanson <jim@amplience.com>
2016-01-23 00:19:50 +0000

    Started making a site in angular

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('teessideHackspaceWebsite', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'mm.foundation', 'toastr']);
+
+})();
