9e5e7895de6ff3fb5131f8fefaafc51fd391226a
Jim Allanson <jim@amplience.com>
2016-03-09 19:28:55 +0000

    Added organisation page

--- src/app/index.module.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  'use strict';
-
-  angular
-    .module('teessideHackspaceWebsite', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'mm.foundation', 'toastr']);
-
-})();
