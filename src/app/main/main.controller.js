3ac2ff953f42a7fcbe13a63558fc59895262c846
Jim Allanson <jim@amplience.com>
2016-03-15 14:22:34 +0000

    Fixed build issue

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -8,9 +8,13 @@
   /** @ngInject */
   function MainController($scope, $http, auth) {
     $scope.isMember = false;
+    $scope.hasAccount = false;
 
     if(auth && auth.isAuthenticated && auth.profile && auth.profile.roles && auth.profile.roles.indexOf('member') != -1) {
       $scope.isMember = true;
+    }
+    if(auth && auth.isAuthenticated) {
+      $scope.hasAccount = true;
     }
   }
 })();
